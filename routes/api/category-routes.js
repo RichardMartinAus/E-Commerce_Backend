9d2800155d9cefbee35f85d8a54a2199c707e9c1
RichardMartinAUS <richard.martin001@gmail.com>
2022-08-20 10:31:36 +1000

    working on last routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -72,43 +72,52 @@ router.put("/:id", async (req, res) => {
     res.status(500).json(err);
   }
 
-  // Product.update(req.body, {
-  //   where: {
-  //     id: req.params.id,
-  //   },
-  // })
-  //   .then((product) => {
-  //     // find all associated tags from ProductTag
-  //     return ProductTag.findAll({ where: { product_id: req.params.id } });
-  //   })
-  //   .then((productTags) => {
-  //     // get list of current tag_ids
-  //     const productTagIds = productTags.map(({ tag_id }) => tag_id);
-  //     // create filtered list of new tag_ids
-  //     const newProductTags = req.body.tagIds
-  //       .filter((tag_id) => !productTagIds.includes(tag_id))
-  //       .map((tag_id) => {
-  //         return {
-  //           product_id: req.params.id,
-  //           tag_id,
-  //         };
-  //       });
-  //     // figure out which ones to remove
-  //     const productTagsToRemove = productTags
-  //       .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-  //       .map(({ id }) => id);
+  // router.put("/:id", async (req, res) => {
+  //   // update a tag's name by its `id` value
+  //   try {
+  //     const tagData = await Tag.update({
+  //       where: {
+  //         id: req.params.id,
+  //       },
+  //     });
 
-  //     // run both actions
-  //     return Promise.all([
-  //       ProductTag.destroy({ where: { id: productTagsToRemove } }),
-  //       ProductTag.bulkCreate(newProductTags),
-  //     ]);
-  //   })
-  //   .then((updatedProductTags) => res.json(updatedProductTags))
-  //   .catch((err) => {
-  //     // console.log(err);
-  //     res.status(400).json(err);
-  //   });
+  //     if (!tagData) {
+  //       res.status(404).json({ message: "No tag found with this id!" });
+  //       return;
+  //     }
+
+  //     res.status(200).json(tagData);
+  //   } catch (err) {
+  //     res.status(500).json(err);
+  //   }
+
+  // router.put("/:book_id", (req, res) => {
+  //   //Calls the update method on the Book model
+  //   Book.update(
+  //     {
+  //       // All the fields you can update and the data attached to the request body.
+  //       title: req.body.title,
+  //       author: req.body.author,
+  //       isbn: req.body.isbn,
+  //       pages: req.body.pages,
+  //       edition: req.body.edition,
+  //       is_paperback: req.body.is_paperback,
+  //     },
+  //     {
+  //       // Gets a book based on the book_id given in the request parameters
+  //       where: {
+  //         book_id: req.params.book_id,
+  //       },
+  //     }
+  //   )
+  //     .then((updatedBook) => {
+  //       res.json(updatedBook);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       res.json(err);
+  //     });
+  // });
 });
 
 router.delete("/:id", async (req, res) => {
